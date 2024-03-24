cf472fd02194a352fa8c0367a38a15fec30b8b46
SimpleMan2022 <aditn689@gmail.com>
2024-03-24 15:29:22 +0700

    second commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require 'function.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= title ?> - Dashboard</title>
+    <title><?= title ?> - ADMIN DASHBOARD</title>
     <?php require 'header.php' ?>
     <script>
         $(document).ready(function () {
